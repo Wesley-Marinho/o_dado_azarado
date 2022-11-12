9a5c52b529a26b1fc29830f5e89c71b91fbd7885
Wesley Marinho <wesleymarinho47@gmail.com>
2022-11-12 07:25:03 -0300

    side bar update

--- src/components/side-bar/sideBar.tsx
+++ src/components/side-bar/sideBar.tsx
@@ -1,10 +1,27 @@
 import "./style.scss";
-import { Container } from "react-bootstrap";
+import { Container, Form, FloatingLabel } from "react-bootstrap";
 export function SideBar() {
   return (
     <>
-      <Container id="sidebarPro"></Container>
-      <Container id="sidebar"></Container>
+      <Container id="sidebarPro">
+        <main className="contentSideBar">
+          <div>
+            <h1>Nome: -</h1>
+            <h5>Classe: -</h5>
+            <h5>Raça: -</h5>
+            <h5>Tendencia: -</h5>
+            <h5>Antecedente: -</h5>
+            <h5>Experiencia: -</h5>
+            <h5>Nivel: -</h5>
+          </div>
+          <hr/>
+        </main>
+      </Container>
+      <Container id="sidebar">
+        <main className="contentSideBar">
+          <></>
+        </main>
+      </Container>
     </>
   );
 }
